9921506265626323eecaef0a480eae0ca936b9b0
Rifqy Hazim H R <rifqyhazim22@gmail.com>
2025-10-02 15:04:38 +0700

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://rifqyhazim22.github.io/web_portofolio_with_astro/',
+  base: '/web_portofolio_with_astro',
+});
